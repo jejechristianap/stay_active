56c2d4233a7e107972b7747b775a46aeb3b37405
Jeremia Christian <jejechristianap@gmail.com>
2021-09-21 13:51:16 +0700

    Add Dagger notes

--- app/src/main/java/com/jejec/stayactive/MainActivity.kt
+++ app/src/main/java/com/jejec/stayactive/MainActivity.kt
@@ -7,5 +7,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**
+         * # What is Dagger?
+         * Dagger is dependency injection
+         *
+         * # What is dependency?
+         * Kotlin object or variable,
+         * if objectA relies on objectB
+         * then objectB is a dependency of objectA
+         * */
     }
 }
