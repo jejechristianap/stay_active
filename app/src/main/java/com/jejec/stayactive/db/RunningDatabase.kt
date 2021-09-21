c815d9f40084be4b846903ad49964ac2c49b4c0b
Jeremia Christian <jejechristianap@gmail.com>
2021-09-21 13:35:59 +0700

    RoomDatabase setup

--- app/src/main/java/com/jejec/stayactive/db/RunningDatabase.kt
+++ app/src/main/java/com/jejec/stayactive/db/RunningDatabase.kt
@@ -1,4 +1,12 @@
 package com.jejec.stayactive.db
 
-class RunningDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(entities = [Run::class], version = 1)
+@TypeConverters(Converters::class)
+abstract class RunningDatabase : RoomDatabase(){
+
+    abstract fun getRunDao(): RunDAO
 }
