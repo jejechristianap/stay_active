123d7f72d204d9c3e8bb2d378d984752af26ddc9
Jeremia Christian <jejechristianap@gmail.com>
2021-10-09 23:47:48 +0700

    NavHost not found fixed

--- app/src/main/java/com/jejec/stayactive/ui/MainActivity.kt
+++ app/src/main/java/com/jejec/stayactive/ui/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.jejec.stayactive.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.jejec.stayactive.R
 import com.jejec.stayactive.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,11 +26,28 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var bind: ActivityMainBinding
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         bind = ActivityMainBinding.inflate(layoutInflater)
-
+        setContentView(bind.root)
         setSupportActionBar(bind.toolbar)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        bind.bottomNavigationView.setupWithNavController(navController)
+
+        navController
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when (destination.id) {
+                    R.id.settingFragment, R.id.runFragment, R.id.statisticFragment -> {
+                        bind.bottomNavigationView.visibility = View.VISIBLE
+                    }
+                    else -> {
+                        bind.bottomNavigationView.visibility = View.GONE
+                    }
+                }
+            }
     }
 }
