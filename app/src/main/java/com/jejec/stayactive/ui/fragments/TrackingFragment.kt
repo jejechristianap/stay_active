bfebe4a778ce66c1e24db51385f0194bc618665a
Jeremia Christian <jejechristianap@gmail.com>
2021-10-10 16:18:49 +0700

    Fragment setup

--- app/src/main/java/com/jejec/stayactive/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/jejec/stayactive/ui/fragments/TrackingFragment.kt
@@ -1,9 +1,22 @@
 package com.jejec.stayactive.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.jejec.stayactive.R
+import com.jejec.stayactive.databinding.FragmentRunBinding
+import com.jejec.stayactive.databinding.FragmentTrackingBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class TrackingFragment : Fragment(R.layout.fragment_statistic) {
+class TrackingFragment : Fragment(R.layout.fragment_tracking) {
+
+    private var _bind: FragmentTrackingBinding? = null
+    private val bind get() = _bind!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _bind = FragmentTrackingBinding.bind(view)
+
+    }
 }
