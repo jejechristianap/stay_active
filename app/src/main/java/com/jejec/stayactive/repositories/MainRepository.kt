616a0b3fb3840ff4c3f852e3c276769a7293bffd
Jeremia Christian <jejechristianap@gmail.com>
2021-10-10 20:13:31 +0700

    Add MainRepository

---
+++ app/src/main/java/com/jejec/stayactive/repositories/MainRepository.kt
@@ -0,0 +1,31 @@
+package com.jejec.stayactive.repositories
+
+import com.jejec.stayactive.db.Run
+import com.jejec.stayactive.db.RunDAO
+import javax.inject.Inject
+
+class MainRepository @Inject constructor(
+    private val runDao: RunDAO
+) {
+    suspend fun insertRun(run: Run) = runDao.insertRun(run)
+
+    suspend fun deleteRun(run: Run) = runDao.deleteRun(run)
+
+    fun getAllRunsSortedByDate() = runDao.getAllRunsSortedByDate()
+
+    fun getAllRunsSortedByDistance() = runDao.getAllRunsSortedByDistanceInMeters()
+
+    fun getAllRunsSortedByTimeInMillis() = runDao.getAllRunsSortedByTimeInMillis()
+
+    fun getAllRunsSortedByAvgSpeed() = runDao.getAllRunsSortedByAvgSpeedInKMH()
+
+    fun getAllRunsSortedByCaloriesBurned() = runDao.getAllRunsSortedByCaloriesBurned()
+
+    fun getTotalAvgSpeed() = runDao.getTotalAvgSpeed()
+
+    fun getTotalDistance() = runDao.getTotalDistance()
+
+    fun getTotalCaloriesBurned() = runDao.getTotalCaloriesBurned()
+
+    fun getTotalTimeInMillis() = runDao.getTotalTimeInMillis()
+}
